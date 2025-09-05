64888f03f03d558e63ede3ccea6afeb770318e0a
asotito <andressotokr1@gmail.com>
2025-09-05 14:52:53 -0500

    feat: implemented saving button desing

--- src/pages/design-interface/DesignInterfaceRender.tsx
+++ src/pages/design-interface/DesignInterfaceRender.tsx
@@ -18,6 +18,7 @@ import { updateProjectName, deleteProject } from '@/services/project.service';
 
 const DesignInterfaceRender: React.FC = () => {
   const navigate = useNavigate();
+  const [isSaving, setIsSaving] = useState<Boolean>(true);
   const { projectId, projectName } = useParams<{ projectId: string; projectName: string }>();
   const [selectedWindow, setSelectedWindow] = useState<Window>();
   const [liveCode, setLiveCode] = useState<string>('');
@@ -92,6 +93,12 @@ const DesignInterfaceRender: React.FC = () => {
         </div>
 
         <div className="w-full flex-grow flex flex-col items-center justify-center bg-[#202123] p-4">
+          <div className={`left-0 w-full flex gap-1 items-center ${isSaving ? "animate-pulse" : ""}`}>
+            <h1 className="text-xs">{isSaving ? "Saving..." : "Saved"}</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+            </svg>
+          </div>
           <WindowInterface
             projectId={projectId ?? ''}
             webSocketCode={liveCode}
