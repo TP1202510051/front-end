02e097cf7de83391cc3e98546742c69bf45911c4
asotito <andressotokr1@gmail.com>
2025-09-23 20:17:39 -0500

    chore: redone thinking icon

--- src/assets/icons/ThinkingIcon.tsx
+++ src/assets/icons/ThinkingIcon.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 export function ThinkingIndicator() {
+  const [toggleText, setToggleText] = useState(true);
+
+  useEffect(()=> {
+    const interval = setInterval(() => {
+      setToggleText((prev) => !prev);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [])
+
   return (
     <div className="flex px-3 mt-4 space-x-1 items-baseline text-[var(--dialog-foreground)]">
-      <span className="text-xs">Pensando</span>
+      <span className="text-xs">{toggleText ? "Pensando" : "Creando"}</span>
       {[0, 0.1, 0.2].map((i) => (
         <div
           key={i}
